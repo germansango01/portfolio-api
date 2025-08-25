085b681330dde645d77bc6a756868128cf5f5755
Germán Contreras <germansango@gmail.com>
2025-08-26 00:58:03 +0200

    create migrations

---
+++ database/migrations/2025_08_26_000707_create_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('post_id')->unsigned()->nullable(false);
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->text('content')->nullable(false);
+            $table->timestamps();
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
