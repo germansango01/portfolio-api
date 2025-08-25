daca19d913c9ae0b527d86dbd1319407bdc8ef96
Germán Contreras <germansango@gmail.com>
2025-08-25 22:38:07 +0200

    remove data

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,16 +5,6 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/**
- * @OA\Schema(
- *     schema="UserResource",
- *     type="object",
- *     @OA\Property(property="id", type="integer", format="int64", description="Identificador único para el usuario"),
- *     @OA\Property(property="name", type="string", description="Nombre del usuario"),
- *     @OA\Property(property="email", type="string", description="Dirección de correo electrónico del usuario"),
- *     @OA\Property(property="photo", type="string", description="Ruta de la foto de perfil del usuario")
- * )
- */
 class UserResource extends JsonResource
 {
     /**
