db22b4b4d12d77ca6b4984aedf4af04891e78cfb
Germán Contreras <germansango@gmail.com>
2025-08-29 00:11:07 +0200

    refactor in BaseController

---
+++ database/migrations/2025_08_28_231649_create_menu_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('title')->nullable(false);
+            $table->string('url')->nullable(false);
+            $table->integer('parent_id')->nullable();
+            $table->integer('position')->nullable(false);
+            $table->foreignId('menu_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_items');
+    }
+}
